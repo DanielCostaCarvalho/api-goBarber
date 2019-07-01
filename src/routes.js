58eb9330cbe10fd11035ff5a5ce15475832262ab
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-07-01 20:14:01 -0300

    Criação de model e loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import Router from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ hello: 'world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Daniel Costa Carvalho',
+    email: 'danielcostacarvalho@gmail.com',
+    password_hash: '1234567890',
+  });
+  res.json(user);
+});
 
 export default routes;
