46f22812b34d798b55d39732a31e6e0ebf87f279
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-07-01 21:54:05 -0300

    Criação: Controller; Rota de cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import Router from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Daniel Costa Carvalho',
-    email: 'danielcostacarvalho@gmail.com',
-    password_hash: '1234567890',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
