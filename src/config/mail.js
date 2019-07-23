b37ef9223112df1a0f75738ad52eb16282f06e4c
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-07-23 10:48:50 -0300

    Adicionando envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a8ce89865364f1',
+    pass: '5327f745302171',
+  },
+  default: {
+    from: 'Equipe goBarber <noreply@gobarber.com>',
+  },
+};
