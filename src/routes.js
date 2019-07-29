b623013f84c457a1deabca1361b58dc480bc5b31
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-07-29 10:17:59 -0300

    adicionando rota de listagem de horarios disponíveis

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvaliableController from './app/controllers/AvaliableController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -23,6 +24,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/avaliable', AvaliableController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
